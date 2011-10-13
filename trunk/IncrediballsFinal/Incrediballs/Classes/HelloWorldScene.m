460d257eba21801df2a9c354409503c553a554fa
rohit4326@gmail.com <rohit4326@gmail.com@aa22aa75-d673-affa-953c-5d253daab858>
2011-10-13 00:21:12 +0000

    Added ad banner to the project

--- trunk/IncrediballsFinal/Incrediballs/Classes/HelloWorldScene.m
+++ trunk/IncrediballsFinal/Incrediballs/Classes/HelloWorldScene.m
@@ -319,4 +319,58 @@ CCMenuItemLabel *item4Text;
 	[[CCDirector sharedDirector] pushScene:[OptionScene node]];
 }
 
+-(void)onEnter
+{
+    NSLog(@"On Enter");
+    // Call the super enter function
+    [super onEnter];
+    
+    addView = [[ADBannerView alloc] initWithFrame:CGRectZero];
+    addView.delegate = self;
+    addView.requiredContentSizeIdentifiers = [NSSet setWithObjects: ADBannerContentSizeIdentifierPortrait, ADBannerContentSizeIdentifierLandscape, nil];
+    addView.currentContentSizeIdentifier = ADBannerContentSizeIdentifierLandscape;
+    
+    [[[CCDirector sharedDirector] openGLView] addSubview:addView];
+    
+    // Transform iAD
+    CGSize windowSize = [[CCDirector sharedDirector] winSize];
+    
+    addView.center = CGPointMake(addView.frame.size.width/2, windowSize.height/2-145);
+    addView.hidden = YES;
+    
+}
+
+
+-(void) onExit
+{
+    NSLog(@"On exit");
+    addView.delegate = nil;
+    [addView removeFromSuperview];
+    [addView release];
+    addView = nil;
+    
+    [super onExit];
+}
+
+
+-(void) bannerViewDidLoadAd:(ADBannerView *)banner
+{
+    NSLog(@"banner view did load ad");
+    addView.hidden = NO;
+}
+
+-(void) bannerView:(ADBannerView *)banner didFailToReceiveAdWithError:(NSError *)error
+{
+    NSLog(@"banner view : did fail to receieve ad with error");
+    addView.hidden = YES;
+}
+
+
+-(void) bannerViewActionDidFinish:(ADBannerView *)banner
+{
+    NSLog(@"banner view : did view action did finish");
+    [[UIApplication sharedApplication] setStatusBarStyle:(UIInterfaceOrientation)[[CCDirector sharedDirector]deviceOrientation]];
+}
+
+
 @end
