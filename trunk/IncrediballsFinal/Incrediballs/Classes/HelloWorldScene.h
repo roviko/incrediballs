347464f3d33a0d8b8fc9581030286633316c2a07
rohit4326@gmail.com <rohit4326@gmail.com@aa22aa75-d673-affa-953c-5d253daab858>
2011-10-13 00:19:26 +0000

    Added add banner

--- trunk/IncrediballsFinal/Incrediballs/Classes/HelloWorldScene.h
+++ trunk/IncrediballsFinal/Incrediballs/Classes/HelloWorldScene.h
@@ -8,11 +8,14 @@
 
 // When you import this file, you import all the cocos2d classes
 #import "cocos2d.h"
+#import <Foundation/Foundation.h>
+#import <iAd/iAd.h>
 
 
 // HelloWorld Layer
-@interface HelloWorld : CCLayer
+@interface HelloWorld : CCLayer<ADBannerViewDelegate>
 {
+    ADBannerView *addView;
 }
 
 // returns a Scene that contains the HelloWorld as the only child
