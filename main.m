49c154edfd6ea9025e37366b64ab42ac6ffde6ac
chintankumar.j.patel@gmail.com <chintankumar.j.patel@gmail.com@aa22aa75-d673-affa-953c-5d253daab858>
2011-11-29 11:04:44 +0000

    Added missing files

---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Incrediballs
+//
+//  Created by student on 2/16/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+	NSAutoreleasePool *pool = [NSAutoreleasePool new];
+	int retVal = UIApplicationMain(argc, argv, nil, @"IncrediballsAppDelegate");
+	[pool release];
+	return retVal;
+}
